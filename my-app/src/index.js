fc609f25b4949f03933f74db3a4d61e1b539f5b3
SCM <hys1693359@gmail.com>
2023-10-10 17:15:03 +0900

    23.10.10 (SCM) : Main.js 가안

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import App_context from './App_context';
+import Main from './Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App_context />
+    <Main/>
   </React.StrictMode>
 );
 
